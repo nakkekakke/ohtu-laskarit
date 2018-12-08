10a6fc701a2a37c7ce09c918971a39207bf7eae2
nakkekakke <juntusen.niko@gmail.com>
2018-12-08 19:54:13 +0200

    lh6-1

---
+++ Laskin/src/main/java/laskin/Nollaa.java
@@ -0,0 +1,34 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class Nollaa implements Komento {
+    private TextField tuloskentta;
+    private TextField syotekentta;
+    private Button nollaa;
+    private Button undo;
+    private Sovelluslogiikka sovellus;
+    
+    public Nollaa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        this.tuloskentta = tuloskentta;
+        this.syotekentta = syotekentta;
+        this.nollaa = nollaa;
+        this.undo = undo;
+        this.sovellus = sovellus;
+    }
+
+    @Override
+    public void suorita() {
+        sovellus.nollaa();
+        nollaa.disableProperty().set(true);
+        undo.disableProperty().set(false);
+    }
+
+    @Override
+    public void peru() {
+        System.out.println("Peruminen valittu");
+    }
+
+}
